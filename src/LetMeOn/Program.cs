2f5a10f1f7655e7a708df3e1fb1eb09f2b74904f
Andrew Lock <andrewlock.net@gmail.com>
2017-12-27 22:03:29 +0000

    Fix title

--- src/LetMeOn/Program.cs
+++ src/LetMeOn/Program.cs
@@ -11,7 +11,7 @@ namespace AspNetCoreInDocker.Web
             var host = WebHost.Start(ctx =>
             {
                 ctx.Response.ContentType = "text/html";
-                return ctx.Response.WriteAsync(@"<!DOCTYPE html><html lang=""en"" xmlns=""http://www.w3.org/1999/xhtml""><head> <meta charset=""utf-8""/> <title>LetMeOn.Ste</title> <style>html, body{width: 100%; height: 100%;}html{display: table;}body{display: table-cell; vertical-align: middle; text-align: center; background: linear-gradient(135deg, rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(125,185,232,1) 100%);}h1{font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color: #fff;}</style></head><body> <h1>Hooray, you're online!</h1></body></html>");
+                return ctx.Response.WriteAsync(@"<!DOCTYPE html><html lang=""en"" xmlns=""http://www.w3.org/1999/xhtml""><head> <meta charset=""utf-8""/> <title>LetMeOn.Site</title> <style>html, body{width: 100%; height: 100%;}html{display: table;}body{display: table-cell; vertical-align: middle; text-align: center; background: linear-gradient(135deg, rgba(30,87,153,1) 0%,rgba(41,137,216,1) 50%,rgba(125,185,232,1) 100%);}h1{font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; color: #fff;}</style></head><body> <h1>Hooray, you're online!</h1></body></html>");
             });
             host.WaitForShutdown();
         }
